c59cebab2935535963589cbc60382861f9f21095
Abhimanyu Chand <ak@BatBook-Pro.local>
2024-12-08 20:03:49 +0530

    Fix Redis connection typing

--- lib/redis.ts
+++ lib/redis.ts
@@ -1,6 +1,9 @@
 import { Redis } from 'ioredis';
 
-// Your Upstash Redis connection URL from the dashboard
-const redis = new Redis(process.env.REDIS_URL);
+if (!process.env.REDIS_URL) {
+  throw new Error('REDIS_URL is not defined');
+}
+
+const redis = new Redis(process.env.REDIS_URL as string);
 
 export default redis;
